d5d8e40cf5fc9f0e7fa03286f3d391c1fe1c6fc8
zenlex <erichrkeil@gmail.com>
2022-02-02 21:01:54 -0600

    comment cleanup

--- src/components/mdview.js
+++ src/components/mdview.js
@@ -1,19 +1,16 @@
 import React, { useEffect } from 'react';
- 
-//Previewer is a container div (id = 'preview') which contains resulting markdown->HTML
+
 function MdView(props) {
 
-//side effect called after render() to inject the parsed markdown into the preview div
-//useEffect used to ensure 'preview' div has been mounted to DOM before trying to edit .innerHTML
   useEffect(() => {
     document.getElementById('preview').innerHTML = props.markdown
   });
 
-  return(
-    <div className = 'wrapper'>
+  return (
+    <div className='wrapper'>
       <h2>Preview:</h2>
       <hr />
-        <div id = 'preview' />
+      <div id='preview' />
     </div>
   )
 
